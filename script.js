81c8d72c306ddf8e99595723373ef58daea109d0
Rachealelias <rachealsamuel708@gmail.com>
2021-08-09 18:10:22 -0400

    commit tree 4e852808b9831ee26b2ac0ab7bb99502a822a913

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-//const h2 = document.createElement('h2');
-//h2.textContent = "This content added by JavaScript"; 
+const container = document.querySelector('.container');
+fetch ('http://localhost:3000/products')
+.then(resp => resp.json())
+.then(data => {
+    //console.log(data)
+    data.forEach(product)   
+})
 
-//document.querySelector('body').appendChild(h2); 
+function product(data){
+    const productContainer = document.createElement('div')
+    productContainer.className = 'product-container'
+    const image = document.createElement('img')
+    const titles = document.createElement('p')
+    titles.className = 'product-title'
+    titles.innerText = data.title
+    const categories = document.createElement('p')
+    categories.innerText = data.category
+    categories.className= 'product-cat'
+    const likeButton = document.createElement('button')
+
+    const like = '👍'
+    const dislike = '👎'
+    likeButton.innerHTML = data.like ? like : dislike
+
+    image.src = data.image
+
+    likeButton.addEventListener('click', (evt) => {
+        likeButton.innerHTML = likeButton.innerHTML === like ? dislike : like
+    })
+    productContainer.append(image, titles, categories, likeButton)
+    container.append(productContainer)
+
+    image.style.height = 400
+    image.style.width = 200
+}
